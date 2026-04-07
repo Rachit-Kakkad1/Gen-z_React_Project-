ce0134a53dcb7777070d23d553e5a16fc7b68ced
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-04-07 09:51:29 +0530

    Add main application entrypoints: App.jsx

---
+++ Gen-Z/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home.jsx"
+import Nav from "./components/Nav.jsx"
+import About from './pages/About.jsx'
+import Career from './pages/Carrer.jsx'
+import Support from './pages/Support.jsx'
+import Contact from './pages/Contact.jsx'
+import FloatingAudioPlayer from './components/FloatingAudioPlayer.jsx'
+
+export default function App() {
+  return (
+    <>
+      <Nav/>
+      <Routes>
+        <Route path='/' element= {<Home/>}/>
+        <Route path='/about' element= {<About/>}/>
+        <Route path='/carrer' element= {<Career/>}/>
+        <Route path='/support' element= {<Support/>}/>
+        <Route path='/contact' element= {<Contact/>}/>
+      </Routes>
+      <FloatingAudioPlayer/>
+    </>
+  )
+}
